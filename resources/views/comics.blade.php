1bc1e6c3b51e464fe6394110659457a632ec64ad
Vitto95 <grimaldi.vitto@gmail.com>
2021-06-22 20:32:11 +0200

    modifiche fino a jumbotron

---
+++ resources/views/comics.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('content')
+
+  <main>
+    <h2>questa è la mia sezione comics</h2>
+  </main>
+
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,52 @@
+<header>
+  <!-- header top -->
+  <div class="header-top">
+    <div class="container">
+      <span> DC POWER VISA </span>
+    <span> ADDITIONAL DC SITES </span>
+    </div>
+  </div>
+  <!-- header bottom -->
+  <div class="header-bottom">
+    <div class="container">
+      <div class="logo-wrapper">
+        <img src="{{ asset('img/dc-logo.png') }}" alt="">
+      </div>
+      <ul class="menu">
+        <li>
+          <a href="#">Characters</a>
+        </li>
+        <li>
+          <a href="#">Comics</a>
+        </li>
+        <li>
+          <a href="#">Movies</a>
+        </li>
+        <li>
+          <a href="#">TV</a>
+        </li>
+        <li>
+          <a href="#">Games</a>
+        </li>
+        <li>
+          <a href="#">Collectibles</a>
+        </li>
+        <li>
+          <a href="#">Videos</a>
+        </li>
+        <li>
+          <a href="#">Fans</a>
+        </li>
+        <li>
+          <a href="#">News</a>
+        </li>
+        <li>
+          <a href="#">Shop</a>
+        </li>
+      </ul>
+      <div class="search">
+        <span>Search...</span>
+      </div>
+    </div>
+  </div>
+</header>
